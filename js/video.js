929b4da6368e3b5fa3ba737293aefbcde4a8a045
hyunji3183 <133857456+hyunji3183@users.noreply.github.com>
2023-06-23 18:24:01 +0900

    UP

--- js/video.js
+++ js/video.js
@@ -17,6 +17,7 @@ function stopPreview(video) {
 
     video.addEventListener("mouseenter", ()=>
     {
+        
         startPreview(video);
     });
     video.addEventListener("mouseleave", ()=>
